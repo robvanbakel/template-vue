8af33210abd4d95b26b72cf2f75e45b6f10c481b
Rob van Bakel <robbakel@gmail.com>
2023-01-21 14:09:23 +0100

    Add Tailwind ESlint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
     "eslint:recommended",
     "@vue/eslint-config-typescript",
     "@vue/eslint-config-prettier",
+    "plugin:tailwindcss/recommended",
   ],
   parserOptions: {
     ecmaVersion: "latest",
